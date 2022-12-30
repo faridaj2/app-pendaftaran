6b13b521cc8b1c9b9b703b09e352ccf4545556ec
faridaj2 <42547920+faridaj2@users.noreply.github.com>
2022-12-30 07:52:31 +0700

    fix js sidenav

--- public/assets/js/sidenav-burger.js
+++ public/assets/js/sidenav-burger.js
@@ -9,7 +9,7 @@ sidenav_trigger.addEventListener("click", function () {
 
   top_bread.classList.toggle("translate-x-[5px]");
   bottom_bread.classList.toggle("translate-x-[5px]");
-  sidenav.classList.toggle("translate-x-0");
+  sidenav.classList.toggle("-translate-x-full");
   sidenav.classList.toggle("shadow-soft-xl");
   sidenav_close_button.classList.toggle('hidden');
 });
@@ -21,7 +21,7 @@ sidenav_close_button.addEventListener("click", function () {
 
 window.addEventListener("click", function (e) {
   if (!sidenav.contains(e.target) && !sidenav_trigger.contains(e.target)) {
-    if (sidenav.classList.contains("translate-x-0")) {
+    if (!sidenav.classList.contains("-translate-x-full")) {
       sidenav_trigger.click();
     }
   }
